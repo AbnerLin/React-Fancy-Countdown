b14b405ec78953faca4cc6e6d1163919129be80b
AbnerLin <shaoyang0805@gmail.com>
2018-07-02 23:22:45 +0800

    Add days, minutes, seconds.

--- src/components/flip-count-down/index.js
+++ src/components/flip-count-down/index.js
@@ -18,7 +18,8 @@ const flipCountDown = (WrappedComponent) => {
       };
 
       this.updateTime = this.updateTime.bind(this);
-      this._callback = this._callback.bind(this);
+      // this._callback = this._callback.bind(this);
+      this.flip = this.flip.bind(this);
     }
 
     componentDidMount() {
@@ -35,21 +36,26 @@ const flipCountDown = (WrappedComponent) => {
        });
     }
 
-    updateTime(seconds) {
+    updateTime(flatSeconds) {
 
       // console.log(DateTimeUtil.getSecondsDef());
       // console.log(parseInt(seconds / DateTimeUtil.getSecondsDef().DAY));
       // console.log(parseInt(seconds % DateTimeUtil.getSecondsDef().DAY / DateTimeUtil.getSecondsDef().HOUR));
       // console.log(parseInt(seconds % DateTimeUtil.getSecondsDef().HOUR / DateTimeUtil.getSecondsDef().MINUTE));
       // console.log(parseInt(seconds % DateTimeUtil.getSecondsDef().MINUTE));
-      // console.log(DateTimeUtil.format(seconds, 'd hh mm ss'));
+      console.log(DateTimeUtil.format(seconds, 'd hh mm ss'));
+      console.log(parseInt(seconds % DateTimeUtil.getSecondsDef().MINUTE));
+
+      /** seconds */
+      // let
+
 
       this.setState({
         time: seconds
       });
     }
 
-    _callback() {
+    flip() {
 
     }
 
@@ -61,36 +67,73 @@ const flipCountDown = (WrappedComponent) => {
           _callback={this._callback}>
           <div>
             { !this.state.due ? this.state.time : this.props.dueElement }
+            <div className="countdown">
 
-          <div className="countdown">
-
-            <div className="block-time">
-
-              <span className="title">HOURS</span>
-              <div className="stage hours hours-1">
-                <span className="top">5</span>
-                <span className="top-back">
-                  4
-                </span>
-                <span className="bottom">4</span>
-                <span className="bottom-back">
-                  5
-                </span>
+              <div className="block-time">
+                <span className="title">DAYS</span>
+                <div className="stage days days-1">
+                  <span className="top">5</span>
+                  <span className="top-back">4</span>
+                  <span className="bottom">4</span>
+                  <span className="bottom-back">5</span>
+                </div>
+                <div className="stage days days-2">
+                  <span className="top">6</span>
+                  <span className="top-back">5</span>
+                  <span className="bottom">5</span>
+                  <span className="bottom-bck">6</span>
+                </div>
               </div>
 
-              <div className="stage hours hours-2">
-                <span className="top">6</span>
-                <span className="top-back">5</span>
-                <span className="bottom">5</span>
-                <span className="bottom-bck">6</span>
+              <div className="block-time">
+                <span className="title">HOURS</span>
+                <div className="stage hours hours-1">
+                  <span className="top">5</span>
+                  <span className="top-back">4</span>
+                  <span className="bottom">4</span>
+                  <span className="bottom-back">5</span>
+                </div>
+                <div className="stage hours hours-2">
+                  <span className="top">6</span>
+                  <span className="top-back">5</span>
+                  <span className="bottom">5</span>
+                  <span className="bottom-bck">6</span>
+                </div>
               </div>
+
+              <div className="block-time">
+                <span className="title">MINUTES</span>
+                <div className="stage minutes minutes-1">
+                  <span className="top">5</span>
+                  <span className="top-back">4</span>
+                  <span className="bottom">4</span>
+                  <span className="bottom-back">5</span>
+                </div>
+                <div className="stage minutes minutes-2">
+                  <span className="top">6</span>
+                  <span className="top-back">5</span>
+                  <span className="bottom">5</span>
+                  <span className="bottom-bck">6</span>
+                </div>
+              </div>
+
+              <div className="block-time">
+                <span className="title">SECONDS</span>
+                <div className="stage seconds seconds-1">
+                  <span className="top">5</span>
+                  <span className="top-back">4</span>
+                  <span className="bottom">4</span>
+                  <span className="bottom-back">5</span>
+                </div>
+                <div className="stage seconds seconds-2">
+                  <span className="top">6</span>
+                  <span className="top-back">5</span>
+                  <span className="bottom">5</span>
+                  <span className="bottom-bck">6</span>
+                </div>
+              </div>
+
             </div>
-
-
-
-          </div>
-
-
           </div>
         </WrappedComponent>
       );
