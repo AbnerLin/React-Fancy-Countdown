7806a241d3539308d969e982bb9ca1729e25bb09
AbnerLin <shaoyang0805@gmail.com>
2018-07-03 01:10:12 +0800

    tmp

--- src/components/flip-count-down/index.js
+++ src/components/flip-count-down/index.js
@@ -43,19 +43,30 @@ const flipCountDown = (WrappedComponent) => {
       // console.log(parseInt(seconds % DateTimeUtil.getSecondsDef().DAY / DateTimeUtil.getSecondsDef().HOUR));
       // console.log(parseInt(seconds % DateTimeUtil.getSecondsDef().HOUR / DateTimeUtil.getSecondsDef().MINUTE));
       // console.log(parseInt(seconds % DateTimeUtil.getSecondsDef().MINUTE));
-      console.log(DateTimeUtil.format(seconds, 'd hh mm ss'));
-      console.log(parseInt(seconds % DateTimeUtil.getSecondsDef().MINUTE));
+      console.log(DateTimeUtil.format(flatSeconds, 'd hh mm ss'));
 
       /** seconds */
-      // let
+      let seconds = parseInt(flatSeconds % DateTimeUtil.getSecondsDef().MINUTE);
+      var seconds1 = document.querySelector('.seconds-1');
+      var seconds2 = document.querySelector('.seconds-2');
+
+      // console.log(seconds);
+      // console.log(seconds1);
+      // console.log(seconds2);
+
+      if (seconds / 10 != seconds1.querySelector('.top').innerHTML) this.flip(seconds1, seconds / 10);
+      if (seconds % 10 != seconds2.querySelector('.top').innerHTML) this.flip(seconds2, seconds % 10);
 
 
       this.setState({
-        time: seconds
+        time: flatSeconds
       });
     }
 
-    flip() {
+    flip(element, value) {
+      console.log(element.querySelector('.top').innerHTML);
+
+
 
     }
 
@@ -78,42 +89,42 @@ const flipCountDown = (WrappedComponent) => {
                   <span className="bottom-back">5</span>
                 </div>
                 <div className="stage days days-2">
-                  <span className="top">6</span>
-                  <span className="top-back">5</span>
-                  <span className="bottom">5</span>
-                  <span className="bottom-bck">6</span>
+                  <span className="top">2</span>
+                  <span className="top-back">1</span>
+                  <span className="bottom">1</span>
+                  <span className="bottom-bck">2</span>
                 </div>
               </div>
 
               <div className="block-time">
                 <span className="title">HOURS</span>
                 <div className="stage hours hours-1">
-                  <span className="top">5</span>
-                  <span className="top-back">4</span>
-                  <span className="bottom">4</span>
-                  <span className="bottom-back">5</span>
+                  <span className="top">4</span>
+                  <span className="top-back">3</span>
+                  <span className="bottom">3</span>
+                  <span className="bottom-back">4</span>
                 </div>
                 <div className="stage hours hours-2">
-                  <span className="top">6</span>
-                  <span className="top-back">5</span>
-                  <span className="bottom">5</span>
-                  <span className="bottom-bck">6</span>
+                  <span className="top">4</span>
+                  <span className="top-back">3</span>
+                  <span className="bottom">3</span>
+                  <span className="bottom-bck">4</span>
                 </div>
               </div>
 
               <div className="block-time">
                 <span className="title">MINUTES</span>
                 <div className="stage minutes minutes-1">
-                  <span className="top">5</span>
-                  <span className="top-back">4</span>
-                  <span className="bottom">4</span>
-                  <span className="bottom-back">5</span>
+                  <span className="top">3</span>
+                  <span className="top-back">2</span>
+                  <span className="bottom">2</span>
+                  <span className="bottom-back">3</span>
                 </div>
                 <div className="stage minutes minutes-2">
-                  <span className="top">6</span>
-                  <span className="top-back">5</span>
-                  <span className="bottom">5</span>
-                  <span className="bottom-bck">6</span>
+                  <span className="top">3</span>
+                  <span className="top-back">2</span>
+                  <span className="bottom">2</span>
+                  <span className="bottom-bck">3</span>
                 </div>
               </div>
 
